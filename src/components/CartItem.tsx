2cf4c48715483f71e8deff65917d49d56afec241
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-06-23 17:17:06 -0400

    created ShoppingCart component. Added functionality to StoreItem buttons.

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,3 @@
+export function CartItem() {
+	return <h1>hi</h1>;
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import { Button, Container, Nav, Navbar as NavbarBS } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 
 export function Navbar() {
+	const { openCart, cartQuantity } = useShoppingCart();
 	return (
 		<NavbarBS sticky='top' className='bg-white shadow-sm mb-3'>
 			<Container>
@@ -16,39 +18,42 @@ export function Navbar() {
 						About
 					</Nav.Link>
 				</Nav>
-				<Button
-					style={{
-						width: '3rem',
-						height: '3rem',
-						position: 'relative',
-					}}
-					variant='outline-primary'
-					className='rounded-circle'
-				>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						width='24'
-						height='24'
-						viewBox='0 0 24 24'
-						fill='currentColor'
-					>
-						<path d='M24 3l-.743 2h-1.929l-3.474 12h-13.239l-4.615-11h16.812l-.564 2h-13.24l2.937 7h10.428l3.432-12h4.195zm-15.5 15c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm6.9-7-1.9 7c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z' />
-					</svg>
-					<div
-						className='rounded-circle bg-danger d-flex justify-content-center align-items-center'
+				{cartQuantity > 0 && (
+					<Button
+						onClick={openCart}
 						style={{
-							color: 'white',
-							width: '1.5rem',
-							height: '1.5rem',
-							position: 'absolute',
-							bottom: 0,
-							right: 0,
-							transform: 'translate(25%,25%)',
+							width: '3rem',
+							height: '3rem',
+							position: 'relative',
 						}}
+						variant='outline-primary'
+						className='rounded-circle'
 					>
-						3
-					</div>
-				</Button>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							width='24'
+							height='24'
+							viewBox='0 0 24 24'
+							fill='currentColor'
+						>
+							<path d='M24 3l-.743 2h-1.929l-3.474 12h-13.239l-4.615-11h16.812l-.564 2h-13.24l2.937 7h10.428l3.432-12h4.195zm-15.5 15c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm6.9-7-1.9 7c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z' />
+						</svg>
+						<div
+							className='rounded-circle bg-danger d-flex justify-content-center align-items-center'
+							style={{
+								color: 'white',
+								width: '1.5rem',
+								height: '1.5rem',
+								position: 'absolute',
+								bottom: 0,
+								right: 0,
+								transform: 'translate(25%,25%)',
+							}}
+						>
+							{cartQuantity}
+						</div>
+					</Button>
+				)}
 			</Container>
 		</NavbarBS>
 	);
